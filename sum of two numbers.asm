7e2533968d45ce707e93670e6f622183b4e7ddf2
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2024-08-20 10:30:05 +0600

    Create sum of two numbers.asm
    Take two input and print the sum

---
+++ sum of two numbers.asm
@@ -0,0 +1,33 @@
+.model small
+.stack 100h
+.code
+
+main proc
+    mov ah,01
+    int 21h 
+    sub al,30h
+    mov bl,al
+    
+    mov ah,2       
+    mov dl,10 ;new line here
+    int 21h  
+    mov dl,13
+    int 21h
+    
+    mov ah,01
+    int 21h
+    sub al,30h 
+    mov cl,al
+    
+    add bl,cl
+    add bl,30h
+   
+   mov ah,2
+   mov dl,10
+   int 21h
+       
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+  
